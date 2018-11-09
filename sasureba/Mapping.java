cc41efa6dd4c84168a7d83f61f4eb1aa229d3d21
merarli <merarli@Merarli12.local>
2018-11-09 15:19:58 +0900

    Mappingクラス作成

---
+++ sasureba/Mapping.java
@@ -0,0 +1,20 @@
+package sasureba;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author merarli
+ */
+public class Mapping {
+    String[] map_data;
+
+    public Mapping(String[] map_data) {
+        this.map_data = map_data;
+    }
+    
+}
