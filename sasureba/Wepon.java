4bc949e26953d0825acfc8b254b9d98871b0d6a2
TakayaKon <kntky10213@yahoo.co.jp>
2018-11-09 15:29:25 +0900

    Wepon作成

---
+++ sasureba/Wepon.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sasureba;
+
+/**
+ *
+ * @author C0116116
+ */
+public class Wepon {
+    
+}
