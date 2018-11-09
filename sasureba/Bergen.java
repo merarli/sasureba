444072ea31115928800ba920faaae2ad3b206f8a
TakayaKon <kntky10213@yahoo.co.jp>
2018-11-09 15:53:29 +0900

    グリムベルゲン先生クラス 作成

---
+++ sasureba/Bergen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sasureba;
+
+/**
+ *
+ * @author C0116116
+ */
+public class Bergen {
+    String name;
+    String talk_start;
+
+    public Bergen(String name, String talk_start) {
+        this.name = name;
+        this.talk_start = talk_start;
+    }
+    
+}
