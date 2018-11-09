2e11900f445795dd34654b9d94980250b9afebde
merarli <merarli@Merarli12.local>
2018-11-09 14:18:14 +0900

    first commit2

---
+++ sasureba/User.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sasureba;
+
+/**
+ *
+ * @author merarli
+ */
+
+
+public class User {
+    double hp_now;
+    double hp_max;
+    double gpa;
+    String name;
+    int atk;
+
+    
+}
