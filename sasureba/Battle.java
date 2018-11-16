99ea45fe484e0abae0c857e5961fe7c5c1b4127c
merarli <merarli@Merarli12.local>
2018-11-16 16:17:55 +0900

    シャベル系のやつけした

--- sasureba/Battle.java
+++ sasureba/Battle.java
@@ -20,21 +20,18 @@ public class Battle {   //田中 亮
     double user_gpa;
     double mob_hp;
     double mob_atk;
-    String mob_talk_start;
-    String mob_talk_end;
+
 
     public Battle() {
 
     }
 
-    public double Battle(double user_hp, double user_atk, double mob_hp, double mob_atk, String mob_talk_start, String mob_talk_end) {
+    public double Battle(double user_hp, double user_atk, double mob_hp, double mob_atk) {
         this.user_hp = user_hp;
         this.user_atk = user_atk;
 //        this.user_gpa = user_gpa;
         this.mob_hp = mob_hp;
         this.mob_atk = mob_atk;
-        this.mob_talk_start = mob_talk_start;
-        this.mob_talk_end = mob_talk_end;
 
         while (this.user_hp == 0 || this.mob_hp == 0) {
 
@@ -117,14 +114,6 @@ public class Battle {   //田中 亮
         return mob_atk;
     }
 
-    public String getMob_talk_start() {
-        return mob_talk_start;
-    }
-
-    public String getMob_talk_end() {
-        return mob_talk_end;
-    }
-
     public void setUser_hp(double user_hp) {
         this.user_hp = user_hp;
     }
@@ -145,12 +134,6 @@ public class Battle {   //田中 亮
         this.mob_atk = mob_atk;
     }
 
-    public void setMob_talk_start(String mob_talk_start) {
-        this.mob_talk_start = mob_talk_start;
-    }
 
-    public void setMob_talk_end(String mob_talk_end) {
-        this.mob_talk_end = mob_talk_end;
-    }
 
 }
