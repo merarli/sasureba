39da72dad25b90376c801cf77c88f2b1b6699a6c
TakayaKon <kntky10213@yahoo.co.jp>
2018-12-07 15:09:23 +0900

    微修正

--- sasureba/Sasureba.java
+++ sasureba/Sasureba.java
@@ -67,65 +67,67 @@ public class Sasureba {//目良
             System.out.println("勝利条件：松下先生を倒そう　敗北条件：勇者の敗北");
         }
 
-        //マップの作成
-        String[][] map_data = {
-            {"----", "----", "----", "----", "----"},
-            {"菊池", "柴田", "----", " B  ", "----"},
-            {"長名", " B  ", "----", " 鍵 ", " 宝 "},
-            {"亀田", "----", " C  ", "----", "宇田"},
-            {"ｸﾞﾘﾑ", "----", " A  ", "----", "松下"},};
+        String key15 = scanner.nextLine();
+        if (key.equals("")) {
+            //マップの作成
+            String[][] map_data = {
+                {"----", "----", "----", "----", "----"},
+                {"菊池", "柴田", "----", " B  ", "----"},
+                {"長名", " B  ", "----", " 鍵 ", " 宝 "},
+                {"亀田", "----", " C  ", "----", "宇田"},
+                {"ｸﾞﾘﾑ", "----", " A  ", "----", "松下"},};
 
-        Mapping map = new Mapping(map_data, 0, 0, 0, 0, 0);
+            Mapping map = new Mapping(map_data, 0, 0, 0, 0, 0);
 
-        System.out.println(map.getMappingString());
+            System.out.println(map.getMappingString());
 
-        //[playerのHPが0より大きい]&[歩数が50以下]間ループ　
+            //[playerのHPが0より大きい]&[歩数が50以下]間ループ　
 //        while (player.getHp_now() > 0 && player.getStep_count() >= 50) {
-        while (map.getSibou_flg() == 0 && map.getHosu() < 50 && map.getBoss_flg() == 0) {
-            System.out.println(map.getHosu());
-            //テストですぐ抜けるようにした
+            while (map.getSibou_flg() == 0 && map.getHosu() < 50 && map.getBoss_flg() == 0) {
+                System.out.println(map.getHosu());
+                //テストですぐ抜けるようにした
 
 //            break;
-            map.idou();
+                map.idou();
 
-        }
+            }
 
-        if (map.getBoss_flg() == 1) {
+            if (map.getBoss_flg() == 1) {
                 System.out.println("松下先生「ぐぅっ！ まさか本当に私を倒すとは……」\n"
                         + "主人公「やった！ これで僕は無事に卒業できる！！」\n");
-            
-            String key8 = scanner.nextLine();
-            if (key.equals("")) {
-                System.out.println("松下先生「口約束とはいえ、約束は約束です。ちゃんと単位の事は考える事にしましょう」\n"
-                        + "主人公「ありがとうございます！！」\n");
-            }
-            String key9 = scanner.nextLine();
-            if (key.equals("")) {
-                System.out.println("――こうして僕の、短くも長い戦いは幕を閉じた。これで僕は必要単位を取得して大手を振って卒業ができる。\n"
-                        + "そう思うと今までの陰鬱な気持ちが一気に晴れていった。" +"\n"+ "そんな清々しい気持ちを持って迎えた翌日、僕は松下先生に呼び出された――\n");
-            }
-            String key10 = scanner.nextLine();
-            if (key.equals("")) {
-                System.out.println("松下先生「あぁ、君。色々と考えたんだけど、やっぱり単位は与えない事になりました」\n"
-                        + "主人公「そ、そんな！？ 約束と違うじゃないですか！！」\n");
-            }
-            String key11 = scanner.nextLine();
-            if (key.equals("")) {
-                System.out.println("松下先生「約束は果たしましたよ。単位を与える事を考えることはしましたから。"+"\n"+ "私は一言も単位をあげる事を約束する、とは言ってませんので、ええ」\n"
-                        + "主人公「あ、あぁ！？ 確かに松下先生はずっと単位をあげる事を考えるとは言ったけど、あげると断言してない！」\n"
-                        + "松下先生「そういう訳で君。来年も頑張ってください」\n"
-                        + "主人公「そ、そんなぁ……」\n");
-            }
-            String key12 = scanner.nextLine();
-            if (key.equals("")) {
-                System.out.println("――こうして僕は来年も東京工科大学の生徒として一年間過ごすことが決まってしまったのだった――");
-            }
-        } else if (map.getHosu() < 0) {
-            System.out.println("留年した");
-        } else if (map.getSibou_flg() == 1) {
-            System.out.println("敗北した");
 
+                String key8 = scanner.nextLine();
+                if (key.equals("")) {
+                    System.out.println("松下先生「口約束とはいえ、約束は約束です。ちゃんと単位の事は考える事にしましょう」\n"
+                            + "主人公「ありがとうございます！！」\n");
+                }
+                String key9 = scanner.nextLine();
+                if (key.equals("")) {
+                    System.out.println("――こうして僕の、短くも長い戦いは幕を閉じた。これで僕は必要単位を取得して大手を振って卒業ができる。\n"
+                            + "そう思うと今までの陰鬱な気持ちが一気に晴れていった。" + "\n" + "そんな清々しい気持ちを持って迎えた翌日、僕は松下先生に呼び出された――\n");
+                }
+                String key10 = scanner.nextLine();
+                if (key.equals("")) {
+                    System.out.println("松下先生「あぁ、君。色々と考えたんだけど、やっぱり単位は与えない事になりました」\n"
+                            + "主人公「そ、そんな！？ 約束と違うじゃないですか！！」\n");
+                }
+                String key11 = scanner.nextLine();
+                if (key.equals("")) {
+                    System.out.println("松下先生「約束は果たしましたよ。単位を与える事を考えることはしましたから。" + "\n" + "私は一言も単位をあげる事を約束する、とは言ってませんので、ええ」\n"
+                            + "主人公「あ、あぁ！？ 確かに松下先生はずっと単位をあげる事を考えるとは言ったけど、あげると断言してない！」\n"
+                            + "松下先生「そういう訳で君。来年も頑張ってください」\n"
+                            + "主人公「そ、そんなぁ……」\n");
+                }
+                String key12 = scanner.nextLine();
+                if (key.equals("")) {
+                    System.out.println("――こうして僕は来年も東京工科大学の生徒として一年間過ごすことが決まってしまったのだった――");
+                }
+            } else if (map.getHosu() < 0) {
+                System.out.println("留年した");
+            } else if (map.getSibou_flg() == 1) {
+                System.out.println("敗北した");
+
+            }
         }
-
     }
 }
